79ebd70ae2f7c17cc046eacfa51ff20af836b056
Ben Wildridge <ben.wildridge@gmail.com>
2022-06-19 02:49:27 +0100

    Add project files.

---
+++ F1DriverMananger/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using F1DriverMananger.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace F1DriverMananger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            client.BaseAddress = new Uri("https://localhost:7266/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+                );
+            InitializeComponent();
+        }
+
+        private void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            this.GetDrivers();
+        }
+
+        private async void GetDrivers()
+        {
+            var response = await client.GetStringAsync("Driver");
+            var drivers = JsonConvert.DeserializeObject<List<Driver>>(response);
+            dgDriverList.DataContext = drivers;
+        }
+    }
+}
---
+++ F1DriverMananger/Models/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1DriverMananger.Models
+{
+    internal class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+        public int Race_Wins { get; set; }
+    }
+}
